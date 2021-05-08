164cff564bea286c86ea268ef13f0e421b423b01
A <itthisakdr@gmail.com>
2021-05-08 18:14:31 +0700

    dispaly completed

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,143 @@
-import React from 'react'
-import Menu from "../shared/Menu"
+import React from "react";
+import { useHistory } from "react-router-dom";
+import Menu from "../shared/Menu";
+import Container from "@material-ui/core/Container"
+import { makeStyles } from "@material-ui/core/styles";
+import SwipeContainer from "./SwipeContainer";
+
+const useStyles = makeStyles(() => ({
+  
+  flexCol: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100vw",
+  },
+  flexBetween: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    height: " 100%",
+    width: "100vw",
+  },
+  row: {
+    padding: "10px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  flex: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  center: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  swipeContainer: {
+    height: "80%",
+    maxWidth: "90%",
+  },
+}));
 
 export default function HomePage() {
+  const history = useHistory();
+  const classes = useStyles();
+
+
+  //TODO –––––––– GET API to get accounts and their info
+  const accounts = [
+    {
+      id: 1,
+      planName: "lite",
+      planId: "1",
+      firstName: "Amy",
+      gender: "female",
+      email: "amy@gmail.com",
+      dob: "2001-09-09",
+      aboutMe: "I am nice because I am veyr very nice and also extremely kind and nice.",
+      instagram: "amylee",
+      sporify: "samy",
+      job: "",
+      company: "",
+      school: "Clerk County College",
+      searchLocation: "",
+      currentLocation: "",
+      lastActive: "2020-09-0900:00:09",
+      showActive: 1,
+      recentlyActive: 1,
+      images: [
+        {
+          image:
+            "https://i.picsum.photos/id/1002/600/900.jpg?hmac=4BSgpJzasHKS9vEgQ_Kn3WUjgvc1sUZv-E10bf1bCyA",
+        },
+        {
+          image:
+            "https://i.picsum.photos/id/277/600/900.jpg?hmac=0SZDnUgJesoCsIFVR9u9uG9hUC3dQOxx0_pgop-aIoY",
+        },
+        {
+          image:
+            "https://i.picsum.photos/id/705/600/900.jpg?hmac=19EE_8IKXcp7maJfLind1IgeEHKHlpbeSbN6o5uydJY",
+        },
+        //GIVE IMAGES IN UPLOADED ORDER
+      ],
+      age: 18,
+      // age: DateTime.now().diff(DateTime.fromISO(this.dob), "years"),
+      
+      //––––––––––––––––––––––––––GENERATE–––––––––––––––––––––––––
+      distance: "6km"
+    },
+    {
+      id: 2,
+      planName: "lite",
+      planId: "1",
+      firstName: "JITTY",
+      gender: "female",
+      email: "amy@gmail.com",
+      dob: "2001-09-09",
+      aboutMe: "I am nice.",
+      instagram: "amylee",
+      sporify: "samy",
+      job: "",
+      company: "",
+      school: "Clerk County College",
+      searchLocation: "",
+      currentLocation: "",
+      lastActive: "2020-09-0900:00:09",
+      images: [
+        {
+          image:
+            "https://i.picsum.photos/id/1002/600/900.jpg?hmac=4BSgpJzasHKS9vEgQ_Kn3WUjgvc1sUZv-E10bf1bCyA",
+        },
+        {
+          image:
+            "https://i.picsum.photos/id/277/600/900.jpg?hmac=0SZDnUgJesoCsIFVR9u9uG9hUC3dQOxx0_pgop-aIoY",
+        },
+        {
+          image:
+            "https://i.picsum.photos/id/705/600/900.jpg?hmac=19EE_8IKXcp7maJfLind1IgeEHKHlpbeSbN6o5uydJY",
+        },
+        //GIVE IMAGES IN UPLOADED ORDER
+      ],
+      age: 18,
+      // age: DateTime.now().diff(DateTime.fromISO(this.dob), "years"),
+    },
+  ];
+
   return (
     <div>
       <Menu />
-      HOMEPAGE
+      <Container className={classes.flexBetween}>
+        <SwipeContainer className={classes.swipeContainer} accounts={accounts} />
+        
+      </Container>
     </div>
   )
 }
---
+++ src/pages/home/SwipeContainer.jsx
@@ -0,0 +1,166 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Fab from "@material-ui/core/Fab";
+import ImageSlider from "./ImageSlider";
+import Paper from "@material-ui/core/Paper";
+import FavoriteRoundedIcon from "@material-ui/icons/FavoriteRounded";
+import StarRoundedIcon from "@material-ui/icons/StarRounded";
+import ReplayRoundedIcon from "@material-ui/icons/ReplayRounded";
+import CloseRoundedIcon from "@material-ui/icons/CloseRounded";
+
+const useStyles = makeStyles(() => ({
+  paper: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    width: "90vw",
+    height: "135vw",
+    borderRadius: "15px",
+  },
+  float: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "90vw",
+    height: "135vw",
+  },
+  rightTap: {
+    width: "45vw",
+    height: "135vw",
+    position: "absolute",
+    top: "0px",
+    right: "0px",
+    fontSize: "3rem",
+    color: "#000",
+    zIndex: "10",
+    cursor: "pointer",
+    userSelect: "none",
+  },
+  leftTap: {
+    width: "45vw",
+    height: "135vw",
+    position: "absolute",
+    top: "0px",
+    left: "0px",
+    fontSize: "3rem",
+    color: "#000",
+    zIndex: "10",
+    cursor: "pointer",
+    userSelect: "none",
+  },
+  slide: {
+    opacity: "0",
+    transitionDuration: "1s ease",
+  },
+  active: {
+    opacity: "1",
+    transitionDuration: "1s",
+    transform: "scale(1.08)",
+  },
+  imageIndexBar: {
+    position: "relative",
+    top: "150px",
+    width: "85vw",
+  },
+  container: {
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  info: {
+    position: "absolute",
+    textAlign: "left",
+  },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    width: "100%",
+    padding: "0px",
+    margin: "10px 0px",
+  },
+  button: {
+    // margin: "10px 5px",
+  },
+}));
+
+export default function SwipeContainer({ accounts }) {
+  const classes = useStyles();
+  const [current, setCurrent] = useState(0);
+
+  const nextSlide = () => {
+    setCurrent(current === accounts.length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? accounts.length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(accounts) || accounts.length <= 0) {
+    return null;
+  }
+
+  return (
+    <div className={classes.container}>
+      <Paper elevation={3} className={classes.paper}>
+        {accounts.map((account, index) => {
+          return (
+            <>
+              {index === current && (
+                <ImageSlider className={classes.paper} account={account} />
+              )}
+            </>
+          );
+        })}
+      </Paper>
+      <Container className={classes.buttonContainer}>
+        <Fab
+          color="primary"
+          className={classes.button}
+          onClick={() => {
+            // REWIND
+            prevSlide();
+          }}
+        >
+          <ReplayRoundedIcon />
+        </Fab>
+        <Fab
+          color="primary"
+          className={classes.button}
+          onClick={() => {
+            // like
+            nextSlide();
+          }}
+        >
+          <FavoriteRoundedIcon />
+        </Fab>
+
+        <Fab
+          color="primary"
+          className={classes.button}
+          onClick={() => {
+            // superlike
+            nextSlide();
+          }}
+        >
+          <StarRoundedIcon />
+        </Fab>
+
+        <Fab
+          color="primary"
+          className={classes.button}
+          onClick={() => {
+            // reject
+            nextSlide();
+          }}
+        >
+          <CloseRoundedIcon />
+        </Fab>
+      </Container>
+    </div>
+  );
+}
